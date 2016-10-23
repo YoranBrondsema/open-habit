3f10c651cdb5cf24323798831bee4c53e9f1929b
Yoran Brondsema <yoran.brondsema@gmail.com>
2016-10-22 17:06:25 -0700

    Disable fixtures.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,35 +3,35 @@ import moment from 'npm:moment';
 
 export default Ember.Route.extend({
   beforeModel() {
-    window.localStorage.clear();
-
-    const store = this.get('store');
-
-    // Create the habit
-    const habit = store.createRecord('habit', {
-      id: '7mvv1',
-      label: "Did you bite your nails today?"
-    });
-
-    // 1 day ago
-    store.createRecord('daily-result', {
-      habit: habit,
-      day: moment().subtract(1, 'days').toDate(),
-      isSuccess: true
-    });
-
-    // 2 days ago
-    store.createRecord('daily-result', {
-      habit: habit,
-      day: moment().subtract(2, 'days').toDate(),
-      isSuccess: false
-    });
-
-    // 4 days ago
-    store.createRecord('daily-result', {
-      habit: habit,
-      day: moment().subtract(4, 'days').toDate(),
-      isSuccess: true
-    });
+    // window.localStorage.clear();
+    //
+    // const store = this.get('store');
+    //
+    // // Create the habit
+    // const habit = store.createRecord('habit', {
+    //   id: '7mvv1',
+    //   label: "Did you bite your nails today?"
+    // });
+    //
+    // // 1 day ago
+    // store.createRecord('daily-result', {
+    //   habit: habit,
+    //   day: moment().subtract(1, 'days').toDate(),
+    //   isSuccess: true
+    // });
+    //
+    // // 2 days ago
+    // store.createRecord('daily-result', {
+    //   habit: habit,
+    //   day: moment().subtract(2, 'days').toDate(),
+    //   isSuccess: false
+    // });
+    //
+    // // 4 days ago
+    // store.createRecord('daily-result', {
+    //   habit: habit,
+    //   day: moment().subtract(4, 'days').toDate(),
+    //   isSuccess: true
+    // });
   }
 });
